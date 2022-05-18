b90a9938c9f9e800f9fe432360a2e725f9607125
tetsuji0617 <pedalclecle@gmail.com>
2022-05-18 23:15:58 +0900

    Add new api

---
+++ src/main/java/bean/Dataset2.java
@@ -0,0 +1,23 @@
+package bean;
+
+import lombok.Data;
+
+@Data
+public class Dataset2 {
+
+	String type;
+
+	String label;
+
+	Integer data[];
+
+	String backgroundColor;
+
+	int backgroundColorRed;
+
+	int backgroundColorBlue;
+
+	int backgroundColorGreen;
+
+	float backgroundColorAlpha;
+}
