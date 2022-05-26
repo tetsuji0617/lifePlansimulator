7056c8b93fd2352ad9352ee6b4d08fc5762dafcc
tetsuji0617 <pedalclecle@gmail.com>
2022-05-26 22:55:52 +0900

    Update refactoring PropertyUtil

--- src/main/java/com/pedalclecle/lifeplan/util/PropertyUtil.java
+++ src/main/java/com/pedalclecle/lifeplan/util/PropertyUtil.java
@@ -11,18 +11,11 @@ public class PropertyUtil {
 
 	private static Properties properties;
 
-	private static String[] incomeTaxRate;
-
 	static {
 		properties = new Properties();
 		try {
-//			InputStream input = Application.class.getResourceAsStream("application.properties");
 			File file = ResourceUtils.getFile("classpath:application.properties");
 			properties.load(new FileInputStream(file));
-			String[] incomeTaxRate = properties.getProperty("income.tax.rate").split(",");
-			for(int i = 0; i < incomeTaxRate.length; i++) {
-				System.out.println(incomeTaxRate[i]);
-			}
 		} catch (IOException e) {
 			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
