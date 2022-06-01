a835bd8c77e21e17d089b06b0db87cf235fa87e0
tetsuji0617 <pedalclecle@gmail.com>
2022-06-01 22:57:59 +0900

    Update add test code

---
+++ src/test/java/com/pedalclecle/lifeplan/controller/LifeSimulatorApiControllerTest.java
@@ -0,0 +1,30 @@
+package com.pedalclecle.lifeplan.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class LifeSimulatorApiControllerTest {
+
+	@Autowired
+	MockMvc mockMvc;
+
+	LifeSimulatorApiController lifeSimulatorApiController;
+
+	@BeforeEach
+	void setUp() {
+		lifeSimulatorApiController= new LifeSimulatorApiController();
+	}
+
+	@Test
+	void testGetHealthInsuranceDeducation() {
+
+	}
+
+}
